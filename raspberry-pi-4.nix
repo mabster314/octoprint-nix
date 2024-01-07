dd2db912263728c8fec06889e47ba392ea8e023d
Max Haland <max@haland.org>
2024-01-07 13:43:48 -0500

    Remove unnecessary config

--- raspberry-pi-4.nix
+++ raspberry-pi-4.nix
@@ -11,16 +11,4 @@
         super.makeModulesClosure (x // { allowMissing = true; });
     })
   ];
-
-  boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-      raspberryPi.firmwareConfig = ''
-        gpu_mem=192
-      '';
-    };
-  };
 }
